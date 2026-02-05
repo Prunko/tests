d93a3238ff4240909bee7e2f5ed1c081e124396c
Vitalii <vs.corporal@gmail.com>
2026-02-05 14:03:40 +0200

    Created new branch & updated code file

--- file2.cs
+++ file2.cs
@@ -6,7 +6,10 @@ namespace file2
     {
         public static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello! What is current time?");
+            DateTime time = new DateTime();
+            time = DateTime.UtcNow;
+            Console.WriteLine("Hello! Current time is " + time.ToString());
         }
     }
 }
