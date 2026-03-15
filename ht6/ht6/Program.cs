dddd0366e9776019df1e304f93f8d59b32ca586b
Vitalii <vs.corporal@gmail.com>
2026-03-15 23:39:41 +0200

    Completed hometask 6

---
+++ ht6/ht6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newWarehouse = new Warehouse();
+
+            newWarehouse.OnItemChanged += (index, name) => Console.WriteLine($"LOG: Item {name} was added in slot #{index}");
+
+            var items = new List<string> { "Toothpaste", "Bullets", "Pacifiers" };
+
+            Predicate<string> isEmpty = item => string.IsNullOrWhiteSpace( item );
+
+            Func<string, string> result = name => name.ToUpper();
+
+            foreach(string s in items)
+            {
+                if ( isEmpty( s ) )
+                {
+                    Console.WriteLine("There's no name of product!");
+                    continue;
+                }
+
+                bool isAdded = false;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if(newWarehouse[i] == null)
+                    {
+                        newWarehouse[i] = result(s);
+
+                        isAdded = true;
+
+                        break;
+                    }
+                }
+
+                if(!isAdded)
+                {
+                    Console.WriteLine("Warehouse is full");
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(newWarehouse[i]))
+                {
+                    Console.WriteLine($"{newWarehouse[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ ht6/ht6/Warehouse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ht4
+{
+    internal class Warehouse
+    {
+        private string[] _items = new string[10];
+
+        public string this[int index]
+        {
+            get => _items[index];
+            set
+            {
+                if (index < _items.Length && index >= 0)
+                {
+                    _items[index] = value;
+
+                    OnItemChanged?.Invoke(index, value);
+                } else
+                {
+                    Console.WriteLine("This index is out of range");
+                }
+            }
+        }
+
+        public event Action<int, string> OnItemChanged;
+    }
+}
