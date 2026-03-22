78944f0fdc341a0fefb08bf9e02041c828cf3a0b
Vitalii <vs.corporal@gmail.com>
2026-03-22 22:25:39 +0200

    Completed hometask 7

---
+++ ht7/ht7/ht7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ht7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTyping = true;
+            HashSet<Student> presentStudents = new HashSet<Student> { };
+            Dictionary<int, Student> students = new Dictionary<int, Student>();
+
+            while (isTyping)
+            {
+                Console.Clear();
+
+                Console.Write("===Lecture 7===\nEnter your full name: ");
+                string studentName = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(studentName))
+                {
+                    Console.WriteLine("Wrong input!");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                var newStudent = new Student(studentName);
+                presentStudents.Add(newStudent);
+                students.Add(newStudent.Id, newStudent);
+                
+                Console.WriteLine("Register another student? (Y/N)");
+                if (Console.ReadLine().ToUpper() == "Y")
+                {
+                    continue;
+                }
+                isTyping = false;
+            }
+
+            var student1 = new Student("Ihor Pylypenko");
+            presentStudents.Add(student1);
+            student1.Id = 19;
+
+            if (presentStudents.Contains(student1))
+            {
+                Console.WriteLine("True");
+            } else 
+            { 
+                Console.WriteLine("False"); 
+            }
+
+            /*
+             Contains() returns false, because field Id affects it's 
+             GetHashCode(), object student1 gets in the wrong bucket
+             and this object gets lost, despite it's physically 
+             present in memory
+            */
+        }
+    }
+}
---
+++ ht7/ht7/ht7/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ht7
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        private static int _counter = 0;
+
+        SortedDictionary<DateTime, int> studentJournal = new SortedDictionary<DateTime, int>();
+
+        public Student(string fullName)
+        {
+            _counter++;
+            this.FullName = fullName;
+            this.Id = _counter;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public string GetStudentName(Dictionary<int, Student> students)
+        {
+            bool isTyping = true;
+            while (isTyping)
+            {
+                int studentId;
+                Student student;
+
+                Console.Write("Enter student's ID: ");
+                if (!int.TryParse(Console.ReadLine(), out studentId))
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+                /*
+                 Method Dictionary.TryGetValue() is faster than List.FirstOrNull(), because
+                 Dictionary.TryGetValue() instantly jumps to value when gets it's key, 
+                 unlike List.FirstOrNull(), which looks at every value until finds the
+                 right one.
+                */
+                if (!students.TryGetValue(studentId, out student))
+                {
+                    Console.WriteLine("Student was not found");
+                    continue;
+                }
+                return student.FullName;
+            }
+            return string.Empty;
+        }
+
+        public void GetStudentJournal()
+        {
+            foreach (var score in studentJournal)
+            {
+                Console.WriteLine($"{score.Key}: {score.Value}");
+            }
+        }
+    }
+}
